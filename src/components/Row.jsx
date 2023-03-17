1dc438f4eeab596460c2765f4599d42b8c9603cb
Joel Watkins <eck087@yahoo.co.uk>
2023-03-17 16:47:30 +0000

    like icon and styling added

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import axios from "axios";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
+  const [like, setLike] = useState(false);
 
   useEffect(() => {
     getMovies();
@@ -32,6 +34,13 @@ const Row = ({ title, fetchURL }) => {
                 <div className="absolute top-0 left-0 h-full w-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                   <p className="text-xs md:text-sm flex justify-center items-center h-full text-center">
                     {movie?.title}
+                  </p>
+                  <p>
+                    {like ? (
+                      <FaHeart className="absolute top-4 left-4 text-gray-300" />
+                    ) : (
+                      <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+                    )}
                   </p>
                 </div>
               </div>
