b0c3babf6b537b182ebb894fb5135ee4df0cd177
Joel Watkins <eck087@yahoo.co.uk>
2023-03-17 16:54:29 +0000

    refactored with new Movie component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
+import Movie from "./Movie";
 import axios from "axios";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
 
   useEffect(() => {
     getMovies();
@@ -21,30 +20,7 @@ const Row = ({ title, fetchURL }) => {
       <div className="relative flex items-center">
         <div id={"slider"}>
           {movies.map((movie, index) => {
-            return (
-              <div
-                className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
-                key={index}
-              >
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
-                  alt={movie?.title}
-                  className="w-full h-auto block"
-                />
-                <div className="absolute top-0 left-0 h-full w-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                  <p className="text-xs md:text-sm flex justify-center items-center h-full text-center">
-                    {movie?.title}
-                  </p>
-                  <p>
-                    {like ? (
-                      <FaHeart className="absolute top-4 left-4 text-gray-300" />
-                    ) : (
-                      <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
-                    )}
-                  </p>
-                </div>
-              </div>
-            );
+            return <Movie movie={movie} key={index} />;
           })}
         </div>
       </div>
