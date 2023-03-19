33e880edaf10c8e9ce98d1ed967be09714f79d03
Joel Watkins <eck087@yahoo.co.uk>
2023-03-19 16:37:52 +0000

    set up saveMovie logic when clicking the heart icon

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useAuthContext } from "../context/auth";
+import { db } from "../firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 const Movie = ({ movie }) => {
+  const { user } = useAuthContext();
   const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+
+  const movieId = doc(db, "users", `${user?.email}`);
+
+  const saveMovie = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieId, {
+        savedMovies: arrayUnion({
+          id: movie.id,
+          title: movie.title,
+          image: movie.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Please log in to save a movie!");
+    }
+  };
 
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
@@ -15,7 +38,7 @@ const Movie = ({ movie }) => {
         <p className="text-xs md:text-sm flex justify-center items-center h-full text-center">
           {movie?.title}
         </p>
-        <p>
+        <p onClick={saveMovie}>
           {like ? (
             <FaHeart className="absolute top-4 left-4 text-gray-300" />
           ) : (
